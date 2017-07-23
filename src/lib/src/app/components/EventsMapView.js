46e77602e9cc6c75f6b60b261dfd7b6140b19200
Vivi Wen <hw2428@columbia.edu>
2017-07-23 16:01:34 -0400

    filter in progress

--- src/lib/src/app/components/EventsMapView.js
+++ src/lib/src/app/components/EventsMapView.js
@@ -3,17 +3,28 @@ import { firebaseDb } from '../proxies/FirebaseProxy';
 import MapView from 'react-native-maps';
 import {
   StyleSheet,
+  View,
+  Text
 } from 'react-native';
 import {
   Container,
+  Button
 } from 'native-base';
 import React, { Component } from 'react';
+import moment from 'moment';
 
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#ebeef0',
+  },
+  filterbar: {
+    backgroundColor: 'white',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    paddingTop: 80,
+    paddingBottom: 20
   },
   title: {
     marginBottom: 20,
@@ -35,6 +46,18 @@ class EventMapView extends Component {
 
   state = {
     events: [],
+    filters: [{
+      name: 'Today',
+      selected: false
+    },
+    {
+      name: 'This Week',
+      selected: false
+    },
+    {
+      name: 'This Weekend',
+      selected: false
+    }]
   };
 
   componentDidMount() {
@@ -83,9 +106,51 @@ class EventMapView extends Component {
     </MapView.Marker>
     )
 
+  _renderFilter = (filter, i) => (
+    <Button bordered info small key={ i } onPress={ () => this.onSelectFilter(filter, i) } style={ { borderColor: '#A9A9A9', backgroundColor: filter.selected ? '#00CED1' : 'white', marginRight: 5, padding: -8 } }>
+      <Text style={ { color: '#A9A9A9' } }>{filter.name}</Text>
+    </Button>
+  )
+  /* TODO: RESET events when click again */
+  onSelectFilter = (filter, index) => {
+    let { filters, events } = this.state;
+
+    filters = this.state.filters.map((f, i) => {
+      if (i === index) f.selected = !f.selected;
+      else f.selected = false;
+      return f
+    })
+    events = this.state.events.filter((e) => {
+      if (!e.endDate) return true;
+      const today = moment();
+
+      if (filter.name === 'Today') {
+        return today.isSame(e.endDate, 'year') && today.isSame(e.endDate, 'month') && today.isSame(e.endDate, 'day');
+      } else if (filter.name === 'This Week') {
+        const currentDay = moment().day();
+        const weekStart = today.subtract(currentDay || 6, 'day');
+        const weekEnd = today.add(1, 'day');
+        return moment(e.endDate).isBetween(weekStart, weekEnd);
+      } else if (filter.name === 'This Weekend') {
+
+      } else return true;
+    })
+    this.setState({
+      filters,
+      events
+    })
+  }
+
   render() {
+    const { filters } = this.state;
+
     return (
       <Container>
+        <View
+          style={ styles.filterbar }
+        >
+          {filters.map((filter, i) => this._renderFilter(filter, i))}
+        </View>
         <MapView
           style={ styles.container }
           showsUserLocation={ true }
