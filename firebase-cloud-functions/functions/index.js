47e5029a5ba0318dfad6c38e7d20a9ca2e1627d3
Marcus Hsu <mh3346@columbia.edu>
2017-08-09 11:07:36 -0400

    Add ESLint check for Firebase cloud function sub-project

--- firebase-cloud-functions/functions/index.js
+++ firebase-cloud-functions/functions/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable consistent-return */
+
 // The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require('firebase-functions');
 // The Firebase Admin SDK to access the Firebase Realtime Database.
